ccb4afa010fc97a819c083b93f75891a55941dd8
sSurachai <s6401012620145@email.kmutnb.ac.th>
2022-10-22 19:04:34 +0700

    Add screen

---
+++ FrontEnd/app/src/main/java/com/example/jobchoice/Add_screen.kt
@@ -0,0 +1,11 @@
+package com.example.jobchoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_screen)
+    }
+}
