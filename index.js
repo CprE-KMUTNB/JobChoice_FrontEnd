6d4816b5a77459da3c2d12d33aca7078d0ee2d52
Makkrapongg <110651269+Makkrapongg@users.noreply.github.com>
2022-10-23 16:09:47 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const { User } = require('./model/user');
