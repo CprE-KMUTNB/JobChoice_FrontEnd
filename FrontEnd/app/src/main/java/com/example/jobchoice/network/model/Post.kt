e9257ee53dcc570b284d41e5ac10d060d90b9f58
Makkrapongg <110651269+Makkrapongg@users.noreply.github.com>
2022-10-24 22:03:25 +0700

    Delete FrontEnd directory

--- FrontEnd/app/src/main/java/com/example/jobchoice/network/model/Post.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jobchoice.network.model
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Post(
-    val email: String,
-    val password: String
-
-)
--- FrontEnd/app/src/main/java/com/example/jobchoice/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jobchoice.utils
-
-
-class Constants {
-    companion object{
-        const val BASE_URL = "https://10.0.2.2:5000/"
-    }
-
-}
